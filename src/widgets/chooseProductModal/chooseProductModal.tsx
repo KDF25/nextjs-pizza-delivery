80c72eda97b2709c0840a3effb0fe7b3209d9ba8
KDF25 <karimov.damir.faridovich@gmail.com>
2025-03-11 16:49:42 +0500

    feat: add productCard adaptive

--- src/widgets/chooseProductModal/chooseProductModal.tsx
+++ src/widgets/chooseProductModal/chooseProductModal.tsx
@@ -7,13 +7,14 @@ import {
   DialogContent,
   DialogDescription,
   DialogTitle,
-  // Drawer,
-  // DrawerContent,
-  // DrawerDescription,
-  // DrawerTitle,
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerTitle,
+  ScrollArea,
 } from '@shared/ui';
 import { useRouter } from 'next/navigation';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './chooseProductModal.module.scss';
 import { ProductForm } from './components';
 
@@ -24,23 +25,34 @@ interface Props {
 
 export const ChooseProductModal: FC<Props> = ({ product, className }) => {
   const router = useRouter();
+  const [isMobile, setIsMobile] = useState(false);
 
-  return (
-    <>
-      <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
-        <DialogTitle style={{ display: 'none' }} />
-        <DialogContent className={cn(styles.content, className)}>
-          <DialogDescription style={{ display: 'none' }} />
+  useEffect(() => {
+    const checkScreenSize = () => setIsMobile(window.innerWidth <= 768);
+    checkScreenSize(); // Устанавливаем начальное значение
+    window.addEventListener('resize', checkScreenSize);
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
+  if (!product) return null;
+
+  return isMobile ? (
+    <Drawer open onOpenChange={() => router.back()}>
+      <DrawerTitle style={{ display: 'none' }} />
+      <DrawerContent className={cn(styles.content, className)}>
+        <DrawerDescription style={{ display: 'none' }} />
+        <ScrollArea className="h-full">
           <ProductForm product={product} onSubmit={() => router.back()} />
-        </DialogContent>
-      </Dialog>
-      {/* <Drawer open={Boolean(product)} onOpenChange={() => router.back()}>
-        <DrawerTitle style={{ display: 'none' }} />
-        <DrawerContent className={cn(styles.content, className)}>
-          <DrawerDescription style={{ display: 'none' }} />
-          <ProductForm product={product} onSubmit={() => router.back()} />
-        </DrawerContent>
-      </Drawer> */}
-    </>
+        </ScrollArea>
+      </DrawerContent>
+    </Drawer>
+  ) : (
+    <Dialog open onOpenChange={() => router.back()}>
+      <DialogTitle style={{ display: 'none' }} />
+      <DialogContent className={cn(styles.content, className)}>
+        <DialogDescription style={{ display: 'none' }} />
+        <ProductForm product={product} onSubmit={() => router.back()} />
+      </DialogContent>
+    </Dialog>
   );
 };
